2dd0b4bb77e72107e12cad3d3dc579c4c0ab8331
AndreaDotta <117829484+S5475373@users.noreply.github.com>
2022-11-27 22:03:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n=0, a=1, b=1, c=0;
+    cin >> n;
+        cout << a << endl << b << endl;
+        for (int i = 2; i <= n; i++) {
+            c=a+b;
+            a=b;
+            b=c;
+
+            if (c<=n){
+                cout << c << endl;
+            }
+        }
+    return 0;
 }
